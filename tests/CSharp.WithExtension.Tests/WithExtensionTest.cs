6a1a8338d22b0c136e106460c1b47dc1f25e24bf
Emmanuel Horrent <ehorrent@gmail.com>
2015-05-20 14:08:41 +0200

    WIP: Màj du projet de test

--- tests/CSharp.WithExtension.Tests/WithExtensionTest.cs
+++ tests/CSharp.WithExtension.Tests/WithExtensionTest.cs
@@ -1,13 +1,33 @@
-﻿using NUnit.Framework;
+﻿using System;
+using NUnit.Framework;
 
 namespace CSharp.WithExtension.Tests
 {
     [TestFixture]
     public class WithExtensionTest
     {
-        [Test]
-        public void TestMethod1()
+        private class MultipleCtor
         {
+            public readonly string A;
+            public readonly string B;
+
+            public MultipleCtor()
+            {
+            }
+
+            public MultipleCtor(string a, string b)
+            {
+                this.A = a;
+                this.B = b;
+            }
+        }
+
+        [Test]
+        [ExpectedException(exceptionType: typeof(InvalidOperationException))]
+        public void With_MultipleCtor_Exception()
+        {
+            var obj = new MultipleCtor();
+            obj.With(current => current.A, "NEW A VALUE");
         }
     }
 }
