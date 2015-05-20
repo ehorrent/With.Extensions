9d847dde171a84e28333a15d1bcbdfae4888ce56
Emmanuel Horrent <ehorrent@gmail.com>
2015-05-20 13:52:25 +0200

    Ajout du projet de test

---
+++ tests/CSharp.WithExtension.Tests/WithExtensionTest.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace CSharp.WithExtension.Tests
+{
+    [TestFixture]
+    public class WithExtensionTest
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
