346d0c3656f4e77920614dbee6575d76c4eef243
Emmanuel Horrent <ehorrent@gmail.com>
2015-10-02 09:28:13 +0200

    Added TestCaseSource attributes to simplify tests

---
+++ src/With.Extensions.Tests/Providers/ConstructorProviderTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using NUnit.Framework;
+using With.Providers;
+
+namespace With.Extensions.Tests.Providers
+{
+    [TestFixture]
+    public class ConstructorProviderTest
+    {
+        public static IEnumerable<Func<ConstructorInfo, Constructor>> TestCases()
+        {
+            yield return ExpressionProviders.BuildConstructor;
+            yield return ReflectionProviders.GetConstructor;
+        }
+
+        [TestCaseSource("TestCases")]
+        [ExpectedException(exceptionType: typeof(ArgumentNullException))]
+        public void GetConstructor_NullArgument_Exception(Func<ConstructorInfo, Constructor> provider)
+        {
+            provider(null);
+        }
+    }
+}
